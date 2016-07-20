7e7562b2210585fd794755050cb5c8e5735a860c
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-07-20 16:13:06 -0600

    Remove from IIFE. Remove uses of js-core.

--- src/query-by-regex.js
+++ src/query-by-regex.js
@@ -1,61 +1,72 @@
-require('core-js/fn/array/from');
+// require('core-js/fn/array/from');
+// require('core-js/fn/array/find');
 
-(function () {
+function nodeListToArray(nodeList) {
+	return [].slice.call(nodeList);
+}
 
-	function normalizeArgs(args) {
-		let attr = null;
-		let regex = args[0];
-
-		if (args.length == 2 && args[1] instanceof RegExp) {
-			attr = args[0];
-			regex = args[1];
-		}
-
-		return {
-			attr: attr,
-			regex: regex
+function findInArray(array, cb) {
+	let length = array.length;
+	for (let i = 0; i < length; i++) {
+		if (cb(array[i])) {
+			return array[i];
 		}
 	}
+}
 
-	function getElements(attr) {
-		let selector = (attr) ? `[${attr}]` : '*';
+function normalizeArgs(args) {
+	let attr = null;
+	let regex = args[0];
 
-		return Array.from(
-			document.querySelectorAll(selector)
-		);
+	if (args.length == 2 && args[1] instanceof RegExp) {
+		attr = args[0];
+		regex = args[1];
 	}
 
-	function openingTag(el) {
-		// The following regex captures the contents of the elements opening tag
-		// <div class="one"> -> `div class="one"`
-		return el.outerHTML.match(/^<((?:[^>"]+|"[^"]*")+)>/)[1];
+	return {
+		attr: attr,
+		regex: regex
+	}
+}
+
+function getElements(attr) {
+	let selector = (attr) ? `[${attr}]` : '*';
+
+	return nodeListToArray(
+		document.querySelectorAll(selector)
+	);
+}
+
+function openingTag(el) {
+	// The following regex captures the contents of the elements opening tag
+	// <div class="one"> -> `div class="one"`
+	return el.outerHTML.match(/^<((?:[^>"]+|"[^"]*")+)>/)[1];
+}
+
+function test(el, attr, regex) {
+	let content = (attr) ? el.getAttribute(attr) : openingTag(el);
+	return regex.test(content);
+}
+
+class QueryByRegex {
+	static one(...args) {
+		args = normalizeArgs(args);
+
+		return findInArray(getElements(args.attr), el => {
+			return test(el, args.attr, args.regex);
+		});
 	}
 
-	function test(el, attr, regex) {
-		let content = (attr) ? el.getAttribute(attr) : openingTag(el);
-		return regex.test(content);
+	static all(...args) {
+		args = normalizeArgs(args);
+
+		return getElements(args.attr)
+			.filter(el => {
+				return test(el, args.attr, args.regex);
+			})
 	}
+}
 
-	class QueryByRegex {
-		static one(...args) {
-			args = normalizeArgs(args);
 
-			return getElements(args.attr)
-				.find(el => {
-					return test(el, args.attr, args.regex);
-				})
-		}
-
-		static all(...args) {
-			args = normalizeArgs(args);
-
-			return getElements(args.attr)
-				.filter(el => {
-					return test(el, args.attr, args.regex);
-				})
-		}
-	}
-
-	window.QueryByRegex = QueryByRegex;
-
-})();
+window.QueryByRegex = QueryByRegex;
+// module.exports = QueryByRegex;
