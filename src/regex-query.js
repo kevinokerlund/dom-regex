b9fe3d12ba97088cec0cba07e34325b2f8fd8441
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-08-10 10:53:17 -0600

    Resolves #1 Add in new method for querying inside of elements `one.inside(...)`, and `all.inside(...)`

--- src/regex-query.js
+++ src/regex-query.js
@@ -1,3 +1,5 @@
+import 'polyfill-array-find.js';
+
 function nodeListToArray(nodeList) {
 	let array = [];
 	let length = nodeList.length;
@@ -7,21 +9,43 @@ function nodeListToArray(nodeList) {
 	return array;
 }
 
-function findInArray(array, cb) {
-	let length = array.length;
-	for (let i = 0; i < length; i++) {
-		if (cb(array[i])) {
-			return array[i];
-		}
+function isDOM(obj) {
+	if ("HTMLElement" in window) {
+		return (obj && obj instanceof HTMLElement);
 	}
+	return !!(obj && typeof obj === "object" && obj.nodeType === 1 && obj.nodeName);
 }
 
-function getElements(attr) {
-	let selector = (attr) ? `[${attr}]` : '*';
+function verifyRegex(possibleRegex) {
+	if (possibleRegex instanceof RegExp) {
+		return true;
+	}
+	throw new TypeError('The regex parameter must be a Regular Expression');
+}
 
-	return nodeListToArray(
-		document.querySelectorAll(selector)
-	);
+function verifyAttributeName(possibleAttribute) {
+	if (!possibleAttribute || typeof possibleAttribute === 'string') {
+		return true;
+	}
+
+	throw new TypeError('The attr parameter must be a non-empty String');
+}
+
+function normalizeSelectorOrNodeListOrArrayOrElement(arg) {
+	if (arg instanceof NodeList) {
+		arg = nodeListToArray(arg);
+	}
+
+	if (Array.isArray(arg)) {
+		return arg.filter(isDOM);
+	}
+	else if (arg !== '' && (typeof arg === 'string' || arg instanceof String)) {
+		return nodeListToArray(
+			document.querySelectorAll(arg)
+		)
+	}
+
+	throw new TypeError('The first param should be a selectorOrDomNode');
 }
 
 function openingTag(el) {
@@ -30,23 +54,63 @@ function openingTag(el) {
 	return el.outerHTML.match(/^<((?:[^>"]+|"[^"]*")+)>/)[1];
 }
 
-function test(el, attr, regex) {
+function test(el, regex, attr) {
 	let content = (attr) ? el.getAttribute(attr) : openingTag(el);
 	return regex.test(content);
 }
 
-let QueryByRegex = {
-	one: (regex, attr) => {
-		return findInArray(getElements(attr), el => {
-			return test(el, attr, regex);
-		});
+function setFindOrFilter(useFilter) {
+	let oldFindOrFilter = Array.prototype._findOrFilter;
+
+	let useThis = (useFilter) ? 'filter' : 'find';
+
+	Array.prototype._findOrFilter = function (...args) {
+		Array.prototype._findOrFilter = oldFindOrFilter;
+		return this[useThis](...args);
+	};
+}
+
+function oneOrAll(findAll, regex, attrName) {
+	verifyRegex(regex);
+	verifyAttributeName(attrName);
+
+	setFindOrFilter(findAll);
+
+	return nodeListToArray(document.querySelectorAll('*'))
+		._findOrFilter(el => test(el, regex, attrName));
+}
+
+function inside(findAll, selectorOrNodeListOrArrayOrElement, regex, attrName) {
+	let arrayOfElements = normalizeSelectorOrNodeListOrArrayOrElement(
+		selectorOrNodeListOrArrayOrElement
+	);
+
+	verifyRegex(regex);
+	verifyAttributeName(attrName);
+
+	setFindOrFilter(findAll);
+
+	return arrayOfElements
+		.map(el => nodeListToArray(el.querySelectorAll('*')))
+		.reduce((a, b) => a.concat(b), [])
+		._findOrFilter(el => test(el, regex, attrName));
+}
+
+const QueryByRegex = {
+	all: function (...args) {
+		return oneOrAll(true, ...args);
 	},
-	all: (regex, attr) => {
-		return getElements(attr)
-			.filter(el => {
-				return test(el, attr, regex);
-			})
+	one: function (...args) {
+		return oneOrAll(false, ...args);
 	}
+};
+
+QueryByRegex.all.inside = function (...args) {
+	return inside(true, ...args);
+};
+
+QueryByRegex.one.inside = function (...args) {
+	return inside(false, ...args) || null;
 };
 
 export default QueryByRegex;
